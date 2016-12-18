6bcde99e1ff67dd46be3ef68d8baae01ef746f78
Ferry Utomo <huferry@gmail.com>
2016-12-18 19:44:33 +0100

    package name added

--- sandbox/example.php
+++ sandbox/example.php
@@ -6,7 +6,7 @@ use YoutubeUrlsGetter\VideoUrl;
 include('../Extractor.php');
 include('../VideoUrl.php');
 
-$url = new VideoUrl("https://www.youtube.com/watch?v=AufbF07OHi4");
+$url = new VideoUrl("https://www.youtube.com/watch?v=ecIWPzGEbFc");
 $v = new Extractor($url);
 
 foreach($v->getVideoData()->getStreams() as $s)
@@ -14,3 +14,4 @@ foreach($v->getVideoData()->getStreams() as $s)
     print("$s->type [$s->quality] Format: $s->itag\n");
     print($s->url."\n\n");
 }
+print($v->getVideoData()->title);
